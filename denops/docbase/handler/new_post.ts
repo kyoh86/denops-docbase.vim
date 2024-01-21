3733962af0a31a4cb461d2605d74cc1099f96cdd
kyoh86 <me@kyoh86.dev>
2024-01-21 20:01:50 +0900

    Update dependencies

--- denops/docbase/handler/new_post.ts
+++ denops/docbase/handler/new_post.ts
@@ -1,8 +1,8 @@
 // New Post Buffer
 
-import type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
-import { execute } from "https://deno.land/x/denops_std@v5.2.0/helper/execute.ts";
-import * as buffer from "https://deno.land/x/denops_std@v5.2.0/buffer/mod.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
+import { execute } from "https://deno.land/x/denops_std@v5.3.0/helper/execute.ts";
+import * as buffer from "https://deno.land/x/denops_std@v5.3.0/buffer/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 import { getLogger } from "https://deno.land/std@0.212.0/log/mod.ts";
 
