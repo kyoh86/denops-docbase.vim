68588e0c40a85b888eafa16ea27f429682e77e5c
GitHub <noreply@github.com>
2024-04-11 09:02:21 +0000

    chore(deps): Update deno dependencies

--- denops/docbase/main.ts
+++ denops/docbase/main.ts
@@ -4,15 +4,15 @@ import {
   input,
 } from "https://deno.land/x/denops_std@v6.4.0/helper/mod.ts";
 import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
-import { join } from "https://deno.land/std@0.221.0/path/mod.ts";
-import { ensureFile } from "https://deno.land/std@0.221.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.222.0/path/mod.ts";
+import { ensureFile } from "https://deno.land/std@0.222.0/fs/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.17.2/mod.ts";
 import {
   ConsoleHandler,
   getLogger,
   RotatingFileHandler,
   setup,
-} from "https://deno.land/std@0.221.0/log/mod.ts";
+} from "https://deno.land/std@0.222.0/log/mod.ts";
 
 import { isOpener, isSearchPostsParams } from "./types.ts";
 import { Client } from "./api/client.ts";
