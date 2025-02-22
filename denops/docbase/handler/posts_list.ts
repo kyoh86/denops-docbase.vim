ca496145d847a056c6ce54d4000a7a1932727a62
GitHub <noreply@github.com>
2025-02-22 09:02:59 +0000

    chore(deps): Update deno dependencies

--- denops/docbase/handler/posts_list.ts
+++ denops/docbase/handler/posts_list.ts
@@ -1,10 +1,10 @@
 // Post List Buffer
 
-import type { Denops } from "jsr:@denops/std@~7.4.0";
-import * as buffer from "jsr:@denops/std@~7.4.0/buffer";
-import * as variable from "jsr:@denops/std@~7.4.0/variable";
-import * as option from "jsr:@denops/std@~7.4.0/option";
-import { batch } from "jsr:@denops/std@~7.4.0/batch";
+import type { Denops } from "jsr:@denops/std@~7.5.0";
+import * as buffer from "jsr:@denops/std@~7.5.0/buffer";
+import * as variable from "jsr:@denops/std@~7.5.0/variable";
+import * as option from "jsr:@denops/std@~7.5.0/option";
+import { batch } from "jsr:@denops/std@~7.5.0/batch";
 import { getLogger } from "jsr:@std/log@~0.224.5";
 import { as, ensure, is } from "jsr:@core/unknownutil@~4.3.0";
 
